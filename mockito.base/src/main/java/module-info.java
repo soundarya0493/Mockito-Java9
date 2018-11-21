343ae7e7f9504e8bc6be47200c4231471bcf230e
soundarya <soundarya0493@gmail.com>
2018-11-20 21:11:40 -0800

    After adding modules

---
+++ mockito.base/src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module mockito.base{
+    requires java.base;
+    exports org.mockito.configuration;
+    exports org.mockito.exceptions;
+    exports org.mockito.hamcrest;
+    exports org.mockito.junit;
+    exports org.mockito.plugins;
+    exports org.mockito.quality;
+    exports org.mockito.runners;
+    exports org.mockito.verification;
+
+    }
---
+++ mockito.base/src/main/java/org.mockito/internal/matchers/Any.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2007 Mockito contributors
+ * This program is made available under the terms of the MIT License.
+ */
+
+package org.mockito.internal.matchers;
+
+import java.io.Serializable;
+
+public class Any implements ArgumentMatcher<Object>, VarargMatcher, Serializable {
+
+    public static final Any ANY = new Any();
+
+    public boolean matches(Object actual) {
+        return true;
+    }
+
+    public String toString() {
+        return "<any>";
+    }
+}
