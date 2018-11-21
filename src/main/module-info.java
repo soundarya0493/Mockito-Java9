d1b5074fa2f03fe24032e53b4f793f0750109ec7
soundarya <soundarya0493@gmail.com>
2018-11-20 19:23:25 -0800

    java module file

---
+++ src/main/module-info.java
@@ -0,0 +1,18 @@
+module org.mockito {
+    requires java.base;
+    exports org.mockito.codegen;
+    exports org.mockito.configuration;
+    exports org.mockito.creation;
+    exports org.mockito.exceptions;
+    exports org.mockito.hamcrest;
+    exports org.mockito.invocation;
+    exports org.mockito.junit;
+    exports org.mockito.listeners;
+    exports org.mockito.mock;
+    exports org.mockito.plugins;
+    exports org.mockito.quality;
+    exports org.mockito.runners;
+    exports org.mockito.session;
+    exports org.mockito.stubbing;
+    exports org.mockito.verification;
+}
